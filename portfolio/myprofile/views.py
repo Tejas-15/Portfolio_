d3f6aa37e406e6d6be469be73539138fe3059ed2
ACE-PROJECTS-21 <aceprojects21@gmail.com>
2022-11-28 16:41:06 +0530

    1

---
+++ portfolio/myprofile/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render (request,'home.html')
+
+def about(request):
+    return render (request,'about.html')
+
+def project(request):
+    return render (request,'project.html')
+
+def contactme(request):
+    return render (request,'contact me.html')
